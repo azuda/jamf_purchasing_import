73efd1f94a5201a36e8812dfdfce76db45ea1018
Aaron Zhang <98193099+azuda@users.noreply.github.com>
2026-04-28 14:04:34 -0600

    add logging

--- run.sh
+++ run.sh
@@ -3,8 +3,16 @@
 PROJECT="$PWD"
 VENV="$PROJECT/.venv/bin/python3"
 
-echo "Script start @ $(date)"
+LOG_DIR="$PROJECT/logs"
+timestamp=$(date '+%Y-%m-%d_%H-%M-%S')
+LOG_FILE="$LOG_DIR/$timestamp.log"
+export LOG_FILE="$LOG_DIR/$timestamp.log"
 
-$VENV run.py
+mkdir -p "$LOG_DIR"
+ls -1t "$LOG_DIR" | tail -n +5 | xargs -I {} rm -f "$LOG_DIR/{}"
 
-echo "\nScript done @ $(date)"
+echo "Script start @ $(date)" >> "$LOG_FILE" 2>&1
+
+$VENV run.py >> "$LOG_FILE" 2>&1
+
+echo "\nScript done @ $(date)" >> "$LOG_FILE" 2>&1
